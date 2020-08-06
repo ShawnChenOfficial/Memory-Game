e1fbbe716cf3542bfbc9eef37b95975caf099975
shawn <2017003399@student.sit.ac.nz>
2020-08-06 20:37:28 +1200

    submit

---
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MemoryGame
+{
+    partial class MemoryGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MemoryGame));
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "back-blue-75-1.png");
+            this.imageList.Images.SetKeyName(1, "clubs-a-75.png");
+            this.imageList.Images.SetKeyName(2, "clubs-2-75.png");
+            this.imageList.Images.SetKeyName(3, "clubs-3-75.png");
+            this.imageList.Images.SetKeyName(4, "clubs-4-75.png");
+            this.imageList.Images.SetKeyName(5, "clubs-5-75.png");
+            this.imageList.Images.SetKeyName(6, "clubs-6-75.png");
+            this.imageList.Images.SetKeyName(7, "clubs-7-75.png");
+            this.imageList.Images.SetKeyName(8, "clubs-8-75.png");
+            this.imageList.Images.SetKeyName(9, "clubs-9-75.png");
+            this.imageList.Images.SetKeyName(10, "clubs-10-75.png");
+            this.imageList.Images.SetKeyName(11, "clubs-j-75.png");
+            this.imageList.Images.SetKeyName(12, "clubs-q-75.png");
+            this.imageList.Images.SetKeyName(13, "clubs-k-75.png");
+            this.imageList.Images.SetKeyName(14, "diamonds-a-75.png");
+            this.imageList.Images.SetKeyName(15, "diamonds-2-75.png");
+            this.imageList.Images.SetKeyName(16, "diamonds-3-75.png");
+            this.imageList.Images.SetKeyName(17, "diamonds-4-75.png");
+            this.imageList.Images.SetKeyName(18, "diamonds-5-75.png");
+            this.imageList.Images.SetKeyName(19, "diamonds-6-75.png");
+            this.imageList.Images.SetKeyName(20, "diamonds-7-75.png");
+            this.imageList.Images.SetKeyName(21, "diamonds-8-75.png");
+            this.imageList.Images.SetKeyName(22, "diamonds-9-75.png");
+            this.imageList.Images.SetKeyName(23, "diamonds-10-75.png");
+            this.imageList.Images.SetKeyName(24, "diamonds-j-75.png");
+            this.imageList.Images.SetKeyName(25, "diamonds-q-75.png");
+            this.imageList.Images.SetKeyName(26, "diamonds-k-75.png");
+            this.imageList.Images.SetKeyName(27, "hearts-a-75.png");
+            this.imageList.Images.SetKeyName(28, "hearts-2-75.png");
+            this.imageList.Images.SetKeyName(29, "hearts-3-75.png");
+            this.imageList.Images.SetKeyName(30, "hearts-4-75.png");
+            this.imageList.Images.SetKeyName(31, "hearts-5-75.png");
+            this.imageList.Images.SetKeyName(32, "hearts-6-75.png");
+            this.imageList.Images.SetKeyName(33, "hearts-7-75.png");
+            this.imageList.Images.SetKeyName(34, "hearts-8-75.png");
+            this.imageList.Images.SetKeyName(35, "hearts-9-75.png");
+            this.imageList.Images.SetKeyName(36, "hearts-10-75.png");
+            this.imageList.Images.SetKeyName(37, "hearts-j-75.png");
+            this.imageList.Images.SetKeyName(38, "hearts-q-75.png");
+            this.imageList.Images.SetKeyName(39, "hearts-k-75.png");
+            this.imageList.Images.SetKeyName(40, "spades-a-75.png");
+            this.imageList.Images.SetKeyName(41, "spades-2-75.png");
+            this.imageList.Images.SetKeyName(42, "spades-3-75.png");
+            this.imageList.Images.SetKeyName(43, "spades-4-75.png");
+            this.imageList.Images.SetKeyName(44, "spades-5-75.png");
+            this.imageList.Images.SetKeyName(45, "spades-6-75.png");
+            this.imageList.Images.SetKeyName(46, "spades-7-75.png");
+            this.imageList.Images.SetKeyName(47, "spades-8-75.png");
+            this.imageList.Images.SetKeyName(48, "spades-9-75.png");
+            this.imageList.Images.SetKeyName(49, "spades-10-75.png");
+            this.imageList.Images.SetKeyName(50, "spades-j-75.png");
+            this.imageList.Images.SetKeyName(51, "spades-q-75.png");
+            this.imageList.Images.SetKeyName(52, "spades-k-75.png");
+            // 
+            // MemoryGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 477);
+            this.Name = "MemoryGame";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
+
---
+++ MemoryGame/MemoryGame/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using PlayingCard;
+
+namespace MemoryGame
+{
+    public partial class MemoryGame : Form
+    {
+        const int CARD_WIDTH = 75;
+        const int CARD_HEIGHT = 107;
+        const int NUM_COL = 8;
+
+        Deck deck;
+        PictureBox[] mycards;
+
+        public MemoryGame()
+        {
+            InitializeComponent();
+        }
+
+        private void MemoryGame_Load(object sender, EventArgs e) // FORM
+        {
+            mycards = new PictureBox[32];
+            deck = new Deck();
+            List<Image> images = new List<Image>();
+
+            for (int i = 1; i <= 52; i++)
+            {
+                images.Add(imageList.Images[i]);
+            }
+
+            deck.AssignImage(images, imageList.Images[0]);
+
+            for (int i = 0; i < 32; i++)
+            {
+                PlayingCards card = deck.DealTopCard();
+                PictureBox box = new PictureBox();
+                box.Height = CARD_HEIGHT;
+                box.Width = CARD_WIDTH;
+                box.Size = new Size(CARD_WIDTH, CARD_HEIGHT);
+                int row = i / NUM_COL;
+                int col = i % NUM_COL;
+                box.Location = new Point(40 + col * CARD_WIDTH, 10 + row * CARD_HEIGHT);
+                mycards[i] = box;
+                box.Image = card.BackImage;
+                box.Tag = card;
+                box.Click += Box_Click1; ;
+                this.Controls.Add(box);
+            }
+        }
+
+        private void Box_Click1(object sender, EventArgs e)
+        {
+            PictureBox box = sender as PictureBox;
+            PlayingCards card = box.Tag as PlayingCards;
+            card.Flip();
+            box.Image = card.GetImage();
+
+            bool check = true;
+            check = !check;
+        }
+    }
+}
